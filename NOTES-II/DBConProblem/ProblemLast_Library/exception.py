fdcc0fd82249cb0c973bd1eb3bec73c48c983a37
Chetan Awari <chetanawari2002@gmail.com>
2026-04-04 00:41:46 +0530

    Aprl 4, n-update

--- NOTES-II/DBConProblem/ProblemLast_Library/exception.py
+++
@@ -1,7 +0,0 @@
-class InvalidLendingIdException(Exception): 
-    def __init__(self, message):
-        super().__init__(message)
-        self.__message=message
-    
-    def get_message(self):
-        return self.__message
